e951dc7b96f3092766ceb7b89ea2150868c4193c
gtraxx <gerits.aurelien@gmail.com>
2019-06-13 10:14:37 +0200

    Update plugin.js (clean)

--- plugin.js
+++ plugin.js
@@ -22,7 +22,7 @@
 (function () {
     var loremipsum = (function () {
         'use strict';
-        //tinymce.PluginManager.requireLangPack("youtube");
+
         tinymce.PluginManager.add("loremipsum", function (editor, url) {
             function li(){
                 return 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
